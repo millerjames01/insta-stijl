3fbcce05a40be44609d3b3a9c7cabf4748450b4e
Jim Miller <miller.james01@gmail.com>
2014-03-18 08:35:02 -0400

    Started laying out system for CompositionGenerator

--- src/main/scala/generators/CompositionGenerator.scala
+++ src/main/scala/generators/CompositionGenerator.scala
@@ -2,13 +2,29 @@ package generators
 
 import org.dupontmanual.image._
 import util.Randomize
+import scala.util.Random
+import CompositionGenerator.DimensionToImage
 
-class CompositionGenerator {
+class CompositionGenerator(height: Int, width: Int, 
+    randomImage: Randomize[DimensionToImage]) {
   
+  protected def generateLines = 0
+
 }
 
 object CompositionGenerator {
+  type DimensionToImage = (Int, Int) => Image
+  type Spot = ((Int, Int), Int, Int)
+  
+  def coloredRectangle(color: Color): DimensionToImage =
+    RectangleFilled(color, _, _)
+  
   val defaultWidth = 400
   val defaultHeight = 500
-  val line = 15
+  val lineWidth = 10
+  val lineSpacing = (Random.nextInt(98) + 2) * 5
+  val numLines = Randomize((1, 2), (2, 4), (3, 2))
+  
+  def tooSmall(spot: Spot): Boolean = 
+    spot._2 >= 50 & spot._3 >= 50 & spot._2 * spot._3 > 5625
 }
